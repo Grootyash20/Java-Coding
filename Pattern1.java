3b72088eb6fba51f8932a577c415bc7fe488a6d0
Yash <yashpundir2001@gmail.com>
2024-06-26 20:36:49 +0530

    All Patterns

--- Pattern1.java
+++ Pattern1.java
@@ -36,7 +36,61 @@ public class Pattern1 {
         }
     }
 
+    public static void pat5(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pat6(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pat7(int n) {
+        for (int i = 0; i < n; i++) {
+            // spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("* ");
+            }
+            // spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pat8(int n) {
+        for (int i = 0; i < n; i++) {
+            // spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 0; j < 2 * n - 2 * i - 1; j++) {
+                System.out.print("* ");
+            }
+            // spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        pat4(5);
+        pat8(5);
     }
 }
