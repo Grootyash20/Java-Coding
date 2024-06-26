ef7fe13f473c70f29a62628820daef5688f9fed6
Yash <yashpundir2001@gmail.com>
2024-06-26 20:18:24 +0530

    Multiple patterns

---
+++ Pattern1.java
@@ -0,0 +1,42 @@
+public class Pattern1 {
+
+    public static void pat1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pat2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pat3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pat4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pat4(5);
+    }
+}
