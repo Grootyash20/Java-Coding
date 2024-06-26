8e54730c14060a84c76cfc02e225a9dd6d30b43d
Yash <yashpundir2001@gmail.com>
2024-06-26 20:43:51 +0530

    All Patterns

--- Pattern1.java
+++ Pattern1.java
@@ -90,7 +90,53 @@ public class Pattern1 {
         }
     }
 
+    public static void pat9(int n) {
+        for (int i = 0; i < n; i++) {
+            // spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("* ");
+            }
+            // spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            // spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 0; j < 2 * n - 2 * i - 1; j++) {
+                System.out.print("* ");
+            }
+            // spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pat10(int n) {
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars = i;
+            if (stars > n) {
+                stars = 2 * n - i;
+            }
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        pat8(5);
+        pat10(5);
     }
 }
