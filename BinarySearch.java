75355a09383a1a06f0ba3adce194b1da3be59a42
Grootyash20 <128805588+Grootyash20@users.noreply.github.com>
2024-06-29 22:16:21 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -18,7 +18,7 @@ public class BinarySearch {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 1, 3, 5, 7, 9, 11 };
+        int[] array = { 1, 3, 5, 7, 9, 11 };
         System.out.println(BSearch(arr, 11));
     }
 }
